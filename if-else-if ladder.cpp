fdce4f61c95ca5b29b2238bc448a76246e7d57df
Sreeja-0 <sreejaippala08@gmail.com>
2024-10-02 15:34:18 +0530

    Add files via upload

---
+++ if-else-if ladder.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        //program to print the grades of students using if else if ladder
+        float marks;
+        cout<<"enter marks: ";
+        cin>>marks;
+        if(marks<25)
+        {
+            cout<<"F";
+        }
+        else if(marks>=25 && marks<=44)
+        {
+            cout<<"E";
+
+        }
+        else if(marks>=45 && marks<=49)
+        {
+            cout<<"D";
+
+        }
+        else if(marks>=50 && marks<=59)
+        {
+            cout<<"C";
+
+        }
+        else if(marks>=60 && marks<=79)
+        {
+            cout<<"B";
+
+        }
+
+        else
+        {
+            cout<<"A";
+
+        }
+
+
+        return 0;
+    }
+
