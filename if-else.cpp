0d3b5917d0ffd0bc919a7acc9ee927ec2e44e013
Sreeja-0 <sreejaippala08@gmail.com>
2024-10-02 15:20:26 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        //program for adult/not
+        int age;
+        cout<<"enter age: ";
+        cin>>age;
+        if(age>=18)
+        {
+            cout<<"adult";
+        }
+
+        else
+        {
+            cout<<"not adult";
+        }
+
+        return 0;
+    }
+
